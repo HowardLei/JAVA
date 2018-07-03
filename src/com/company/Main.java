6bf3b2f9362da3d5de7a3e7f6308a544537eb66d
HowardLei <410905481@qq.com>
2018-07-03 18:28:33 +0800

    new one

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("王尼玛");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+        for (int u = 0; u < 10; u++) {
+            System.out.println(u);
         }
     }
 }
