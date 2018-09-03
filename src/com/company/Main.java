7d8f0d781717db1a8b915cf3b6cc9bf651ec09da
HowardLei <410905481@qq.com>
2018-09-03 12:57:58 +0800

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("王尼玛");
-        for (int u = 0; u < 10; u++) {
-            System.out.println(u);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java");
         }
+    }
+    public static void j() {
+        System.out.println("");
     }
 }
