c850f53d7b6d0143a4280ca673e56f2050e80b09
HowardLei <410905481@qq.com>
2018-09-20 11:10:21 +0800

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,22 @@
 package com.company;
 
-
 public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("Welcome to Java");
         }
-    }
-    public static void j() {
-        System.out.println("");
+        String[] allJLines = {"    J", "    J", "J   J", " J J "};
+        String[] allALines = {"   A   ", "  A A  ", " AAAAA ", "A     A"};
+        String[] allVLines = {"V     V", " V   V ", "  V V  ", "   V   "};
+        for (int i = 0; i < 4; i++) {
+            String a = allJLines[i] + " " + allALines[i] + " " + allVLines[i] + " " + allALines[i];
+            System.out.println(a);
+        }
+        int res = 0;
+        for (int i = 1; i < 10; i++) {
+            res += i;
+        }
+        System.out.println(res);
     }
 }
