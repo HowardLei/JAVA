9ba3d1233460577844d8822e5db79e8b14e12bb5
HowardLei <410905481@qq.com>
2018-12-17 12:06:48 +0800

    该写点击事件了

--- src/com/company/ContentJFrame.java
+++ src/com/company/ContentJFrame.java
@@ -1,19 +1,88 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class ContentJFrame extends JFrame {
-    private JComplex complex1, complex2;
+public class ContentJFrame extends JFrame implements ActionListener {
+    private JComplex complex1, complex2, complex3, complex4;
+    private JComboBox<String> box1, box2;
+    private JButton button;
+
     public ContentJFrame() {
         super("复数表达式计算");
-        var container = this.getContentPane();
-        this.setBounds(300, 300, 300, 300);
+        Container container = this.getContentPane();
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
+        this.setLayout(new FlowLayout(FlowLayout.CENTER));
+        this.setBounds(100, 100, 300, 300);
+        this.box1 = new JComboBox<String>();
+        container.add(box1);
+        this.box2 = new JComboBox<String>();
+        container.add(box2);
+        this.setComplexData(container);
+        this.button = new JButton("=");
+        container.add(this.button);
+        this.button.addActionListener(this);
+    }
 
+    private void setFrame() {
+        int margin = 10;
+        this.complex1.setBounds(30, 0, 250, margin);
+        this.complex2.setBounds(30, margin , 250, margin);
+        this.complex3.setBounds(30, margin * 3, 250, margin);
+        this.complex4.setBounds(30, margin * 5, 250, margin);
+    }
+    /**
+     * 设置复数框中的数据
+     * @param container 需要存储数据的容器
+     * */
+    private void setComplexData(Container container) {
+        this.complex1 = new JComplex();
+        this.complex1.setVisible(true);
+        container.add(complex1);
+
+        this.complex2 = new JComplex();
+        this.complex2.setVisible(true);
+        container.add(complex2);
+
+        container.add(this.box1);
+        this.setButtonData(box1);
+
+        this.complex3 = new JComplex();
+        this.complex3.setVisible(true);
+        container.add(complex3);
+
+        container.add(this.box2);
+        this.setButtonData(box2);
+
+        this.complex4 = new JComplex();
+        this.complex4.setVisible(true);
+        container.add(complex4);
+    }
+
+    private void setButtonData(JComboBox<String> box) {
+        String[] arr = {"+", "-", "*", "/"};
+        for (String s : arr) {
+            box.addItem(s);
+        }
+        this.getContentPane().add(box);
+        box.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(this.button)) {
+            // 1、先获得转化数据
+            var num1 = new Complex(Integer.parseInt(this.complex1.getTextField1().getText()), Integer.parseInt(this.complex1.getTextField2().getText()));
+            var num2 = new Complex(Integer.parseInt(this.complex2.getTextField1().getText()), Integer.parseInt(this.complex2.getTextField2().getText()));
+            var num3 = new Complex(Integer.parseInt(this.complex3.getTextField1().getText()), Integer.parseInt(this.complex3.getTextField2().getText()));
+            // 2、根据选项的按钮进行计算
+            // 3、将答案返回回来
+        }
     }
 
     public static void main(String[] args) {
         new ContentJFrame();
     }
-
 }
--- src/com/company/JComplex.java
+++ src/com/company/JComplex.java
@@ -1,69 +1,32 @@
 package com.company;
 
 import javax.swing.*;
-import java.awt.*;
 
-public class JComplex extends JFrame {
+public class JComplex extends JPanel {
     private JTextField textField1, textField2;
     private JLabel label1, label2;
-    private Complex complex1, complex2;
 
     public JComplex() {
-        super("复数表达式计算");
         this.setBounds(200, 200, 300, 300);
-        Container container = this.getContentPane();
         this.setData();
-        this.setFrame();
-        JComponent[] arr = {this.textField1, this.textField2, this.label1, this.label2};
-        for (JComponent component : arr) {
-            container.add(component);
-            component.setVisible(true);
-        }
-        this.setLayout(null);
-        this.setVisible(true);
-        this.setResizable(false);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-    }
-
-    private void setFrame() {
-        int margin = 10;
-        int textField1X = 50;
-        int textField1Y = margin;
-        int textField1H = 20;
-        int textField1W = 50;
-        Rectangle text1Frame = new Rectangle(textField1X, textField1Y, textField1W, textField1H);
-        this.textField1.setBounds(text1Frame);
-
-        int label1W = 10;
-        int label1H = textField1H;
-        int label1X = (int) text1Frame.getMaxX() + margin;
-        int label1Y = textField1Y;
-        Rectangle label1Frame = new Rectangle(label1X, label1Y, label1W, label1H);
-        this.label1.setBounds(label1Frame);
-
-        int textField2W = textField1W;
-        int textField2H = textField1H;
-        int textField2X = (int) label1Frame.getMaxX() + margin;
-        int textField2Y = textField1Y;
-        var textField2Frame = new Rectangle(textField2X, textField2Y, textField2W, textField2H);
-        this.textField2.setBounds(textField2Frame);
-
-        int label2W = label1W;
-        int label2H = label1H;
-        int label2X = (int) textField2Frame.getMaxX() + margin;
-        int label2Y = textField2Y;
-        this.label2.setBounds(label2X, label2Y, label2W, label2H);
     }
 
     private void setData() {
-        this.textField1 = new JTextField();
-        this.textField2 = new JTextField();
+        this.textField1 = new JTextField(5);
         this.label1 = new JLabel("+");
-        this.label2 = new JLabel("i", SwingConstants.CENTER);
+        this.textField2 = new JTextField(5);
+        this.label2 = new JLabel("i", SwingConstants.LEFT);
+        this.add(textField1);
+        this.add(label1);
+        this.add(textField2);
+        this.add(label2);
     }
 
-    public static void main(String[] args) {
-        new JComplex();
+    public JTextField getTextField1() {
+        return textField1;
+    }
+
+    public JTextField getTextField2() {
+        return textField2;
     }
 }
