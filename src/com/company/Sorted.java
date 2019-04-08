52c7484d6c2b402aea9a8cdd3509de911fd186a7
ITCenter <410905481@qq.com>
2019-04-08 20:41:01 +0800

    排序算法（未实现）

--- src/com/company/Sorted.java
+++ src/com/company/Sorted.java
@@ -1,13 +1,40 @@
 package com.company;
 
-public class Sorted<T>{
+public class Sorted<T extends Number> {
+
+    private T[] numbers;
+
+    public Sorted(T[] arr) {
+        this.numbers = arr;
+    }
 
     public static <T> void QuickSort(T[] value, int left, int right) {
         if (left < right) {
+            var Left = left;
+            var Right = right;
+            var temp = value[1];
+            var element = value[left];
+            while (left < right) {
+                // 判断的时候记得调用 compareTo 方法
+                while (value[right] && left < right) {
+                    right--;
+                }
+                while (left < right &&) {
+                    left++;
+                }
+                if (left < right) {
+                    temp = value[left];
+                    value[left] = value[right];
+                    value[right] = temp;
+                }
+            }
+            QuickSort(value, Left, left - 1);
+            QuickSort(value, right + 1, Right);
         }
     }
 
     public static void main(String[] args) {
-
+        var array = new Sorted<Integer>(new Integer[]{2, 32, 323, 3});
     }
+
 }
