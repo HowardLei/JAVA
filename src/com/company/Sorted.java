a05f5e5deb3343562688f3a31ef63af307b3d2b6
HowardLei <410905481@qq.com>
2019-04-12 10:05:07 +0800

    排序成功

--- src/com/company/Sorted.java
+++ src/com/company/Sorted.java
@@ -3,9 +3,10 @@ package com.company;
 public class Sorted<T extends Number> {
     private T[] numbers;
 
-    public Sorted(T[] arr) {
+    Sorted(T[] arr) {
         this.numbers = arr;
     }
+
     public <T extends Number> void QuickSort(int left, int right) {
         if (left < right) {
             var Left = left;
@@ -13,7 +14,7 @@ public class Sorted<T extends Number> {
             var temp = this.numbers[1];
             var element = this.numbers[left];
             while (left < right) {
-                // FIXME: 判断的逻辑并不完善，需要判断各个类的数据
+                // FIXME: 判断的逻辑并不完善，需要判断各个类的数据。这个地方应该判断各个类的数据类型再进行决定
                 while (numbers[right].doubleValue() > element.doubleValue() && left < right) {
                     right--;
                 }
@@ -32,11 +33,15 @@ public class Sorted<T extends Number> {
     }
 
     public static void main(String[] args) {
-        var numbers = new Integer[]{2, 32, 323, 3};
-        var array = new Sorted<Integer>(numbers);
-        array.QuickSort(0, numbers.length - 1);
-        for (var integer : numbers) {
+        var demo = new Integer[]{2, 32, 323, 3, 234, 342, 4};
+        var array = new Sorted<>(demo);
+        for (Integer integer : demo) {
             System.out.println(integer);
+        }
+        System.out.println("------");
+        array.QuickSort(0, demo.length - 1);
+        for (var number : demo) {
+            System.out.println(number);
         }
     }
 }
